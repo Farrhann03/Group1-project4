a37162b1dff0350da71f2f2dd0c5e0b3bf99a272
Alex Hong <alexhongcy@hotmail.com>
2022-06-16 21:53:20 +0800

    added the slide out menu not final

--- supperApp/src/screens/HomeScreen.js
+++ supperApp/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { SafeAreaView, StatusBar, StyleSheet, View, ScrollView, Text, ImageBackground, FlatList, TextInput, Dimensions } from "react-native";
+import React, { useRef, useState } from 'react';
+import { Animated, Image, SafeAreaView, StatusBar, StyleSheet, View, ScrollView, Text, ImageBackground, FlatList, TextInput, Dimensions } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import COLORS from "../consts/colors";
@@ -7,8 +7,30 @@ import places from '../consts/places';
 import SignUpScreen from '../screens/SignInSignUp/SignUpScreen'
 import recommend from "../consts/recommended";
 const {width} = Dimensions.get('screen');
+import profile from '../assets/profile.png'
+import home from '../assets/home.png';
+import search from '../assets/search.png';
+import notifications from '../assets/bell.png';
+import settings from '../assets/settings.png';
+import logout from '../assets/logout.png';
+// Menu
+import menu from '../assets/menu.png';
+import close from '../assets/close.png';
+
+// Photo
+import photo from '../assets/photo.jpg';
 
 const HomeScreen = ({navigation}) => {
+    const [currentTab, setCurrentTab] = useState("Home");
+    // To get the curretn Status of menu ...
+    const [showMenu, setShowMenu] = useState(false);
+  
+    // Animated Properties...
+  
+    const offsetValue = useRef(new Animated.Value(0)).current;
+    // Scale Intially must be One...
+    const scaleValue = useRef(new Animated.Value(1)).current;
+    const closeButtonOffset = useRef(new Animated.Value(0)).current;
     const categoryIcons = [
         <Icon name="fastfood" size={25} color={COLORS.primary2} />,
         <Icon name="local-offer" size={25} color={COLORS.primary2} />,
@@ -39,15 +61,17 @@ const Card = ({place}) => {
         <ImageBackground
             style={style.cardImage}
             source={place.image}>
+                <View style={{backgroundColor: COLORS.white, borderRadius: 10}}>
                 <Text 
                     style={{
-                        color: COLORS.white, 
+                        color: COLORS.primary2,
                         fontSize: 20, 
                         fontWeight: 'bold',
                         marginTop: 10,
                     }}>
                     {place.name}
                 </Text>
+                </View>
                 <View style={{flexDirection: 'row'}}>
                                 <Icon name='star' size={20} color={COLORS.white} />
                                 <Text style={{marginLeft: 5, color:COLORS.white}}>
@@ -113,7 +137,120 @@ const Card = ({place}) => {
      )
  }
 
-    return <SafeAreaView style={{flex:1, backgroundColor: COLORS.white}}>
+    return (
+    
+        <SafeAreaView style={style.container}>
+
+        <View style={{ justifyContent: 'flex-start', padding: 15 }}>
+          <Image source={profile} style={{
+            width: 60,
+            height: 60,
+            borderRadius: 10,
+            marginTop: 8
+          }}></Image>
+  
+          <Text style={{
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: 'white',
+            marginTop: 20
+          }}>Jenna Ezarik</Text>
+  
+          <TouchableOpacity>
+            <Text style={{
+              marginTop: 6,
+              color: 'white'
+            }}>View Profile</Text>
+          </TouchableOpacity>
+  
+          <View style={{ flexGrow: 1, marginTop: 50 }}>
+            {
+              // Tab Bar Buttons....
+            }
+  
+            {TabButton(currentTab, setCurrentTab, "Home", home)}
+            {TabButton(currentTab, setCurrentTab, "Search", search)}
+            {TabButton(currentTab, setCurrentTab, "Notifications", notifications)}
+            {TabButton(currentTab, setCurrentTab, "Settings", settings)}
+  
+          </View>
+  
+          <View>
+            {TabButton(currentTab, setCurrentTab, "LogOut", logout)}
+          </View>
+  
+        </View>
+  
+        {
+          // Over lay View...
+        }
+  
+        <Animated.View style={{
+          flexGrow: 1,
+          backgroundColor: 'white',
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          paddingHorizontal: 15,
+          paddingVertical: 20,
+          borderRadius: showMenu ? 15 : 0,
+          // Transforming View...
+          transform: [
+            { scale: scaleValue },
+            { translateX: offsetValue }
+          ]
+        }}>
+  
+          {
+            // Menu Button...
+          }
+  
+          <Animated.View style={{
+            transform: [{
+              translateY: closeButtonOffset
+            }]
+          }}>
+            <TouchableOpacity onPress={() => {
+              // Do Actions Here....
+              // Scaling the view...
+              Animated.timing(scaleValue, {
+                toValue: showMenu ? 1 : 0.88,
+                duration: 300,
+                useNativeDriver: true
+              })
+                .start()
+  
+              Animated.timing(offsetValue, {
+                // YOur Random Value...
+                toValue: showMenu ? 0 : 230,
+                duration: 300,
+                useNativeDriver: true
+              })
+                .start()
+  
+              Animated.timing(closeButtonOffset, {
+                // YOur Random Value...
+                toValue: !showMenu ? -30 : 0,
+                duration: 300,
+                useNativeDriver: true
+              })
+                .start()
+  
+              setShowMenu(!showMenu);
+            }}>
+  
+              <Image source={showMenu ? close : menu} style={{
+                width: 20,
+                height: 20,
+                tintColor: 'black',
+                marginTop: 40,
+  
+              }}></Image>
+  
+            </TouchableOpacity>
+           
         <StatusBar translucent={false} backgroundColor={COLORS.white}/>
         <View style={style.header}>
             <LogIN />
@@ -158,8 +295,54 @@ const Card = ({place}) => {
                     renderItem={({item}) => <RecommendedCard recommend={item}/>} />
             </View>
         </ScrollView>
-    </SafeAreaView>;
-};
+
+            
+          </Animated.View>
+  
+        </Animated.View>
+  
+      </SafeAreaView>
+    );
+  }
+  
+  // For multiple Buttons...
+  const TabButton = (currentTab, setCurrentTab, title, image) => {
+    return (
+  
+      <TouchableOpacity onPress={() => {
+        if (title == "LogOut") {
+          // Do your Stuff...
+        } else {
+          setCurrentTab(title)
+        }
+      }}>
+        <View style={{
+          flexDirection: "row",
+          alignItems: 'center',
+          paddingVertical: 8,
+          backgroundColor: currentTab == title ? 'white' : 'transparent',
+          paddingLeft: 13,
+          paddingRight: 35,
+          borderRadius: 8,
+          marginTop: 15
+        }}>
+  
+          <Image source={image} style={{
+            width: 25, height: 25,
+            tintColor: currentTab == title ? "#D95FAA" : "white"
+          }}></Image>
+  
+          <Text style={{
+            fontSize: 15,
+            fontWeight: 'bold',
+            paddingLeft: 15,
+            color: currentTab == title ? "#D95FAA" : "white"
+          }}>{title}</Text>
+  
+        </View>
+      </TouchableOpacity>
+    );
+  }
 
 const style = StyleSheet.create({
     header:{
@@ -213,6 +396,7 @@ const style = StyleSheet.create({
         padding: 10,
         overflow: 'hidden',
         borderRadius: 10,
+        color: "rgba(0,0,0,10)"
     },
     rmCardImage: {
         width: width - 40,
@@ -221,6 +405,12 @@ const style = StyleSheet.create({
         borderRadius: 10, 
         overflow: 'hidden',
         padding: 10
-    }
+    },
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.primary2,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+    },
 });
 export default HomeScreen
